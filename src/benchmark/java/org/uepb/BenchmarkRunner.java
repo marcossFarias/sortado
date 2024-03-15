e8de43b214fb2ea009061c84e3b888d0caaef45b
marcosFarias <contact.marcossouzacg2004@proton.me>
2024-03-14 23:39:48 -0300

    export results to csv

--- src/benchmark/java/org/uepb/BenchmarkRunner.java
+++ src/benchmark/java/org/uepb/BenchmarkRunner.java
@@ -2,6 +2,9 @@ package org.uepb;
 
 public class BenchmarkRunner {
     public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
+        org.openjdk.jmh.Main.main(new String[]{
+                "-rf", "csv",
+                "-rff", "./benchmarks.csv"
+        });
     }
 }
