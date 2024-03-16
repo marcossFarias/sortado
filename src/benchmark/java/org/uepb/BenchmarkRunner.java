0bd1dc4d58d79f518cc5d972f5bef52db58d1021
marcosFarias <contact.marcossouzacg2004@proton.me>
2024-03-16 12:27:59 -0300

    log the BenchmarkRunner output into a file

--- src/benchmark/java/org/uepb/BenchmarkRunner.java
+++ src/benchmark/java/org/uepb/BenchmarkRunner.java
@@ -1,10 +1,31 @@
 package org.uepb;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class BenchmarkRunner {
     public static void main(String[] args) throws Exception {
+        String fileName = "BenchmarkRunner_output.txt";
+        FileOutputStream outputFile = new FileOutputStream(fileName);
+        PrintStream fileStream = new PrintStream(outputFile);
+        PrintStream consoleStream = System.out;
+
+        PrintStream dualStream = new PrintStream(new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                consoleStream.write(b);
+                fileStream.write(b);
+            }
+        });
+
+        System.setOut(dualStream);
         org.openjdk.jmh.Main.main(new String[]{
                 "-rf", "csv",
-                "-rff", "./benchmarks.csv"
+                "-rff", "./BenchmarkRunner.csv"
         });
+
+        outputFile.close();
     }
 }
