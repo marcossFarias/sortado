e201ff2f3aa95996382c2e612c2945eb83a9d90b
marcosFarias <contact.marcossouzacg2004@proton.me>
2024-03-19 22:39:21 -0300

    continuando a migração

--- src/benchmark/java/org/uepb/model/algorithms/sorting/selection/OptimizedSelectionSortBenchmark.java
+++ src/benchmark/java/org/uepb/model/algorithms/sorting/selection/OptimizedSelectionSortBenchmark.java
@@ -1,33 +1,10 @@
 package org.uepb.model.algorithms.sorting.selection;
 
-import org.openjdk.jmh.annotations.*;
-import org.uepb.model.algorithms.sorting.bubble.OptimizedBubbleSort;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.uepb.model.algorithms.sorting.SortingBenchmark;
 
-import java.util.concurrent.TimeUnit;
 
-import static org.uepb.utils.ArrayUtils.generateRandomArray;
-
-@State(Scope.Benchmark)
-@Fork(value = 3)
-@Warmup(iterations = 3)
-@Timeout(time = 10)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.SECONDS)
-public class OptimizedSelectionSortBenchmark {
-    @Param({
-            "100",   // 1,000
-            "10000",  // 10,000
-            "100000"  // 100,000
-    })
-    private int ARRAY_SIZE;
-
-    @Setup
-    public void setup() {
-        inputArray = generateRandomArray(ARRAY_SIZE);
-    }
-
-    private double[] inputArray;
-
+public class OptimizedSelectionSortBenchmark extends SortingBenchmark {
     @Benchmark
     public double[] benchmark() {
         OptimizedSelectionSort optimizedSelectionSort = new OptimizedSelectionSort();
