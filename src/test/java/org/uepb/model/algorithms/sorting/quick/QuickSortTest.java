17197e61a3c07f7a9a85c12a2b3dd4d7f9909315
MateusRufinoFerreira <mateus27.rufino@gmail.com>
2024-03-21 21:51:41 -0300

    teste do quicksort

---
+++ src/test/java/org/uepb/model/algorithms/sorting/quick/QuickSortTest.java
@@ -0,0 +1,29 @@
+package org.uepb.model.algorithms.sorting.quick;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.uepb.model.algorithms.sorting.SortingTest;
+
+public class QuickSortTest extends SortingTest {
+    @BeforeEach
+    public void setup() {
+        super.setup();
+        sort = new QuickSort();
+    }
+
+    // Functionality Tests
+    @Test
+    public void testNumberOfSwaps() {
+        double[] input = {5, 3, 1, 4, 2};
+        sort.sort(input);
+        Assertions.assertEquals(7, sort.getNumberOfSwaps());
+    }
+
+    @Test
+    public void testNumberOfComparisons() {
+        double[] input = {5, 3, 1, 4, 2};
+        sort.sort(input);
+        Assertions.assertEquals(10, sort.getNumberOfComparisons());
+    }
+}
