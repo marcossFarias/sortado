bc65664171365e2b3d90c04df53342a7fdc855f7
marcosFarias <contact.marcossouzacg2004@proton.me>
2024-03-23 19:10:05 -0300

    add time and date when exports csv file

--- src/benchmark/java/org/uepb/BenchmarkRunner.java
+++ src/benchmark/java/org/uepb/BenchmarkRunner.java
@@ -4,15 +4,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import static org.uepb.utils.TimeUtils.dateTimeFormatter;
+
 
 public class BenchmarkRunner {
     public static void main(String[] args) throws Exception {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
-        LocalDateTime now = LocalDateTime.now();
-
-        String fileName = "BenchmarkRunner_terminal_output" + dtf.format(now) + ".txt";
+        String fileName = "BenchmarkRunner_terminal_output" + dateTimeFormatter() + ".txt";
 
         FileOutputStream outputFile = new FileOutputStream(fileName);
         PrintStream fileStream = new PrintStream(outputFile);
@@ -29,7 +27,7 @@ public class BenchmarkRunner {
         System.setOut(dualStream);
         org.openjdk.jmh.Main.main(new String[]{
                 "-rf", "csv",
-                "-rff", "./BenchmarkRunner_default_output" + dtf.format(now) + ".csv"
+                "-rff", "./BenchmarkRunner_default_output" + dateTimeFormatter() + ".csv"
         });
 
         outputFile.close();
