df609426b93cd74e57dd30eaf6065258c2727b69
marcosFarias <contact.marcossouzacg2004@proton.me>
2024-03-23 19:35:15 -0300

    removendo a lógica de verificar numero repedidos, a função que gera os números ja garante isso

--- src/main/java/org/uepb/utils/ArrayUtils.java
+++ src/main/java/org/uepb/utils/ArrayUtils.java
@@ -12,7 +12,7 @@ public class ArrayUtils {
         }
         return array;
     }
-// unicos
+
     public static int[] generateUniqueIntArray(int minValue, int maxValue, int size) {
         Random random = new Random();
 
@@ -30,31 +30,5 @@ public class ArrayUtils {
         }
 
         return result;
-    }
-
-    public static Map<Integer, Integer> checkRepeatedNumbers(int[] array) {
-        Map<Integer, Integer> numberCount = new HashMap<>();
-        for (int num : array) {
-            if (numberCount.containsKey(num)) {
-                numberCount.put(num, numberCount.get(num) + 1);
-            } else {
-                numberCount.put(num, 1);
-            }
-        }
-        return numberCount;
-    }
-
-    public static void main(String[] args) {
-        int[] array = (generateUniqueIntArray(Integer.MIN_VALUE, Integer.MAX_VALUE, 1_000_000));
-        FileUtils.printArrayToFile(array, "./test-array-1M");
-        System.out.println(array.length);
-
-        int[] array2 = (generateUniqueIntArray(Integer.MIN_VALUE, Integer.MAX_VALUE, 2_000_000));
-        FileUtils.printArrayToFile(array2, "./test-array-2M");
-        System.out.println(array2.length);
-
-        int[] array3 = (generateUniqueIntArray(Integer.MIN_VALUE, Integer.MAX_VALUE, 3_000_000));
-        FileUtils.printArrayToFile(array3, "./test-array-3M");
-        System.out.println(array3.length);
     }
 }
