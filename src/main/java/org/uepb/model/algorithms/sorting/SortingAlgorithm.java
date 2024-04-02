db8f87bbf01ba6d5876d5cdec9e60c76a9e12052
marcosFarias <contact.marcossouzacg2004@proton.me>
2024-04-02 19:29:25 -0300

    escopo modificado para private

--- src/main/java/org/uepb/model/algorithms/sorting/SortingAlgorithm.java
+++ src/main/java/org/uepb/model/algorithms/sorting/SortingAlgorithm.java
@@ -3,22 +3,41 @@ package org.uepb.model.algorithms.sorting;
 import java.util.concurrent.atomic.LongAdder;
 
 public abstract class SortingAlgorithm {
-    protected final LongAdder numberOfComparisons = new LongAdder();
-    protected final LongAdder numberOfSwaps = new LongAdder();
+    private final LongAdder numberOfComparisons = new LongAdder();
+    private final LongAdder numberOfSwaps = new LongAdder();
 
-    public SortingAlgorithm() {
-    }
-
+    /**
+     * Returns the number of comparisons performed during the last sort operation.
+     *
+     * @return the number of comparisons
+     */
     public long getNumberOfComparisons() {
         return numberOfComparisons.longValue();
     }
 
+    /**
+     * Returns the number of swaps performed during the last sort operation.
+     *
+     * @return the number of swaps
+     */
     public long getNumberOfSwaps() {
         return numberOfSwaps.longValue();
     }
 
+    /**
+     * Sorts the given array and returns the sorted array.
+     *
+     * @param array the array to be sorted
+     * @return the sorted array
+     */
     public abstract double[] sort(double[] array);
 
+    /**
+     * Measures the execution time of the sorting algorithm on the given array.
+     *
+     * @param array the array to be sorted
+     * @return the execution time in nanoseconds
+     */
     public long executionTime(double[] array) {
         long startTime = System.nanoTime();
         sort(array);
@@ -27,10 +46,16 @@ public abstract class SortingAlgorithm {
         return endTime - startTime;
     }
 
+    /**
+     * Increments the number of comparisons performed.
+     */
     protected void incrementComparisons() {
         numberOfComparisons.increment();
     }
 
+    /**
+     * Increments the number of swaps performed.
+     */
     protected void incrementSwaps() {
         numberOfSwaps.increment();
     }
