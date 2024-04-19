ac649f56cd96274707130e2aa97ccc65e21c18b8
AlissomSilva <rodriguesalissom13@gmail.com>
2024-04-19 16:19:21 -0300

    Update QuickSort.java

--- src/main/java/org/uepb/model/algorithms/sorting/quick/QuickSort.java
+++ src/main/java/org/uepb/model/algorithms/sorting/quick/QuickSort.java
@@ -1,41 +1,48 @@
 package org.uepb.model.algorithms.sorting.quick;
 
-import org.uepb.model.algorithms.sorting.SortingAlgorithm;
+import java.util.concurrent.RecursiveAction;
 
-public class QuickSort extends SortingAlgorithm {
+public class QuickSort extends RecursiveAction {
+    private final double[] array;
+    private final int left;
+    private final int right;
+
+    public QuickSort(double[] array, int left, int right) {
+        this.array = array;
+        this.left = left;
+        this.right = right;
+    }
+
     @Override
-    public double[] sort(double[] array) {
-        quick(array, 0, array.length - 1);
-        return array;
-    }
-    private void quick(double[] array, int left, int right){
-        if(left < right){
-            int pivot = partition(array, left, right);
-            quick(array, left, pivot - 1);
-            quick(array, pivot + 1, right);
+    protected void compute() {
+        if (left < right) {
+            // Parte que envolve a recursão e o fork-join
+            int pivotIndex = partition(array, left, right);
+            QuickSort leftTask = new QuickSort(array, left, pivotIndex - 1);
+            QuickSort rightTask = new QuickSort(array, pivotIndex + 1, right);
+            leftTask.fork(); // Fork da tarefa da esquerda
+            rightTask.fork(); // Fork da tarefa da direita
+            leftTask.join(); // Espera a tarefa da esquerda terminar
+            rightTask.join(); // Espera a tarefa da direita terminar
         }
-
     }
-    private int partition(double[] array, int left, int right){
+
+    private int partition(double[] array, int left, int right) {
         double pivot = array[right];
         int i = left - 1;
         for (int j = left; j < right; j++) {
-            // Se o elemento atual é menor ou igual ao pivô
             if (array[j] <= pivot) {
                 i++;
-
-                // Troca array[i] e array[j]
                 double temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
         }
-
-        // Troca array[i+1] e array[right] (ou seja, o pivot)
         double temp = array[i + 1];
         array[i + 1] = array[right];
         array[right] = temp;
-
         return i + 1;
     }
+
+
 }
