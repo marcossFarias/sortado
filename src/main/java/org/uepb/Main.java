25c705306613bc8740ba05dff0bb93eabbc7d2d2
marcossFarias <marcossouzacg2004@proton.me>
2024-04-22 12:08:59 -0300

    example main

--- src/main/java/org/uepb/Main.java
+++ src/main/java/org/uepb/Main.java
@@ -1,28 +1,33 @@
 package org.uepb;
 
-import org.uepb.controller.algorithms.sorting.SortingController;
-import org.uepb.controller.utils.ArrayController;
 import org.uepb.model.algorithms.sorting.SortingAlgorithm;
 import org.uepb.model.algorithms.sorting.bubble.BubbleSort;
-import org.uepb.model.algorithms.sorting.bubble.OptimizedBubbleSort;
+import org.uepb.model.algorithms.sorting.counting.CountingSort;
 import org.uepb.model.algorithms.sorting.insertion.InsertionSort;
 import org.uepb.model.algorithms.sorting.merge.MergeSort;
 import org.uepb.model.algorithms.sorting.quick.QuickSort;
-import org.uepb.model.algorithms.sorting.selection.OptimizedSelectionSort;
 import org.uepb.model.algorithms.sorting.selection.SelectionSort;
-import org.uepb.service.ArrayService;
-import org.uepb.view.algorithms.sorting.SortingComparisonTerminal;
 
-import java.util.*;
 
-import static java.util.Arrays.sort;
-import static org.uepb.utils.ArrayUtils.generateRandomArray;
-import static org.uepb.utils.ArrayUtils.generateUniqueIntArray;
+import java.io.*;
+
 import static org.uepb.utils.FileUtils.readArrayFromFile;
-import static org.uepb.utils.FileUtils.writeArrayToFile;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        double[] array = readArrayFromFile("");
 
+        SortingAlgorithm[] sortingAlgorithms = {
+                new BubbleSort(),
+                new CountingSort(),
+                new InsertionSort(),
+                new MergeSort(),
+                new QuickSort(),
+                new SelectionSort()
+        };
+
+        for (SortingAlgorithm algorithm : sortingAlgorithms) {
+            algorithm.sort(array);
+        }
     }
 }
