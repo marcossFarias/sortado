0f8aa6807375151876a90a1ac8e8bd8a262ac02d
marcossFarias <marcossouzacg2004@proton.me>
2024-04-25 19:21:00 -0300

    hotfix para os indices do counting sort

--- src/main/java/org/uepb/model/algorithms/sorting/CountingSort.java
+++ src/main/java/org/uepb/model/algorithms/sorting/CountingSort.java
@@ -1,34 +1,45 @@
 package org.uepb.model.algorithms.sorting;
 
+import java.util.Arrays;
 
 public class CountingSort<T extends Comparable<T>> implements SortingAlgorithm<T> {
+
     @Override
     public void sort(T[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+
         T max = array[0];
+        T min = array[0];
+
         for (T t : array) {
             if (t.compareTo(max) > 0) {
                 max = t;
             }
-        }
-
-        int maxValue = max.compareTo(array[0]) + 1;
-        int[] countArray = new int[maxValue];
-
-        for (T t : array) {
-            countArray[t.compareTo(array[0])]++;
-        }
-
-        int index = 0;
-        for (int i = 0; i < countArray.length; i++) {
-            while (countArray[i] > 0) {
-                array[index++] = getValueAtIndex(array, i);
-                countArray[i]--;
+            if (t.compareTo(min) < 0) {
+                min = t;
             }
         }
-    }
 
-    @SuppressWarnings("unchecked")
-    private T getValueAtIndex(T[] array, int index) {
-        return array[index];
+        int range = max.compareTo(min) + 1;
+        int[] countArray = new int[range];
+
+        for (T t : array) {
+            countArray[t.compareTo(min)]++;
+        }
+
+        for (int i = 1; i < range; i++) {
+            countArray[i] += countArray[i - 1];
+        }
+
+        T[] sortedArray = Arrays.copyOf(array, array.length);
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            sortedArray[countArray[array[i].compareTo(min)] - 1] = array[i];
+            countArray[array[i].compareTo(min)]--;
+        }
+
+        System.arraycopy(sortedArray, 0, array, 0, array.length);
     }
 }
