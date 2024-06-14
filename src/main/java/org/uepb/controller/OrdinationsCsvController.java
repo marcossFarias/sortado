0861d4e7c25f87600f3f79ea96dea8fe55664ace
marcossFarias <marcossouzacg2004@proton.me>
2024-06-13 21:45:24 -0400

    ADD controller for ordinations

---
+++ src/main/java/org/uepb/controller/OrdinationsCsvController.java
@@ -0,0 +1,71 @@
+package org.uepb.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.uepb.model.algorithms.InsertionSort;
+import org.uepb.model.algorithms.MergeSort;
+import org.uepb.model.algorithms.SortingAlgorithm;
+import org.uepb.service.SortingService;
+
+/**
+ * This class is responsible for managing sorting algorithms and their usage in
+ * CSV operations.
+ */
+public class OrdinationsCsvController {
+  private Map<String, SortingAlgorithm> sortAlgorithms;
+
+  /**
+   * Constructor for creating an instance of OrdinationsCsvController.
+   * Initializes the sortAlgorithms map with sorting algorithms.
+   */
+  public OrdinationsCsvController() {
+    this.sortAlgorithms = new HashMap<>();
+
+    sortAlgorithms.put("bubble", new InsertionSort());
+    sortAlgorithms.put("quick", new MergeSort());
+  }
+
+  /**
+   * Sorts a CSV file by the specified column using the given sorting algorithm
+   * and writes the sorted data to a new CSV file.
+   *
+   * @param inputFile    The path to the input CSV file.
+   * @param outputFile   The path to the output CSV file where sorted data will be
+   *                     written.
+   * @param columnToSort The name of the column to sort.
+   * @param algorithm    The sorting algorithm to be used.
+   */
+  public void sortCsvFileByColumnWithAlgorithm(
+      String inputFile,
+      String outputFile,
+      String columnToSort,
+      SortingAlgorithm algorithm) {
+    SortingService sortingService = new SortingService(algorithm);
+    sortingService.sortCsvFileByColumn(inputFile, outputFile, columnToSort);
+  }
+
+  /**
+   * Sorts a CSV file by the specified column using each sorting algorithm and
+   * writes the sorted data to a new CSV file.
+   *
+   * @param inputFile    The path to the input CSV file.
+   * @param outputFile   The path to the output CSV file where sorted data will be
+   *                     written.
+   * @param columnToSort The name of the column to sort.
+   */
+  public void sortCsvFileByColumnForAllAlgorithms(
+      String inputFile,
+      String outputFile,
+      String columnToSort) {
+    for (Map.Entry<String, SortingAlgorithm> entry : sortAlgorithms.entrySet()) {
+      String algorithmName = entry.getKey();
+      SortingAlgorithm algorithm = entry.getValue();
+
+      sortCsvFileByColumnWithAlgorithm(
+          inputFile,
+          outputFile + "_" + algorithmName + ".csv",
+          columnToSort,
+          algorithm);
+    }
+  }
+}
